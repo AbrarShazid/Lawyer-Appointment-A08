1774bd149c2aafa1185bfb21682f16a58dc1fcf6
Abrar Agmain Shazid <abraragmain75@gmail.com>
2025-04-24 01:19:42 +0600

    .

--- src/Components/Counting.jsx
+++ src/Components/Counting.jsx
@@ -5,15 +5,10 @@ import thirdImg from "../assets/success-patients.png";
 import fourthImg from "../assets/success-staffs.png";
 
 import CountUp from "react-countup";
-import { useInView } from "react-intersection-observer";
+
+const isClient = typeof window !== "undefined";
 
 const Counting = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
-
-  const isClient = typeof window !== "undefined";
   return (
     <div className="mulish">
       <div className="text-center space-y-4">
@@ -27,16 +22,12 @@ const Counting = () => {
       </div>
 
       {/* card part  */}
-
-      <div
-        ref={ref}
-        className="mt-4 md:mt-8 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6"
-      >
-        <div className="rounded-2xl bg-[rgba(15,15,15,0.05)] border border-[rgba(15,15,15,0.15)]  px-5 py-4 md:px-10 md:py-8">
+      <div className="mt-4 md:mt-8 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="rounded-2xl bg-[rgba(15,15,15,0.05)] border border-[rgba(15,15,15,0.15)] px-5 py-4 md:px-10 md:py-8">
           <img className="h-16 w-16 mb-1" src={firstImg} alt="" />
 
-          {inView && isClient &&(
-            <CountUp start={0} end={199} duration={3}>
+          {isClient && (
+            <CountUp start={0} end={199} duration={6}>
               {({ countUpRef }) => (
                 <div className="flex text-[#0F0F0F] text-[40px] font-extrabold items-center">
                   <span ref={countUpRef} />
@@ -53,8 +44,8 @@ const Counting = () => {
 
         <div className="rounded-2xl bg-[rgba(15,15,15,0.05)] border border-[rgba(15,15,15,0.15)] px-5 py-4 md:px-10 md:py-8">
           <img className="h-16 w-16 mb-1" src={secImg} alt="" />
-          {inView && isClient && (
-            <CountUp start={0} end={467} duration={5}>
+          {isClient && (
+            <CountUp start={0} end={467} duration={10}>
               {({ countUpRef }) => (
                 <div className="flex text-[#0F0F0F] text-[40px] font-extrabold items-center">
                   <span ref={countUpRef} />
@@ -71,8 +62,8 @@ const Counting = () => {
 
         <div className="rounded-2xl bg-[rgba(15,15,15,0.05)] border border-[rgba(15,15,15,0.15)] px-5 py-4 md:px-10 md:py-8">
           <img className="h-16 w-16 mb-1" src={thirdImg} alt="" />
-          {inView && isClient &&(
-            <CountUp start={0} end={1900} duration={7}>
+          {isClient && (
+            <CountUp start={0} end={1900} duration={17}>
               {({ countUpRef }) => (
                 <div className="flex text-[#0F0F0F] text-[40px] font-extrabold items-center">
                   <span ref={countUpRef} />
@@ -88,8 +79,8 @@ const Counting = () => {
 
         <div className="rounded-2xl bg-[rgba(15,15,15,0.05)] border border-[rgba(15,15,15,0.15)] px-5 py-4 md:px-10 md:py-8">
           <img className="h-16 w-16 mb-1" src={fourthImg} alt="" />
-          {inView && isClient &&(
-            <CountUp start={0} end={300} duration={4}>
+          {isClient && (
+            <CountUp start={0} end={300} duration={8}>
               {({ countUpRef }) => (
                 <div className="flex text-[#0F0F0F] text-[40px] font-extrabold items-center">
                   <span ref={countUpRef} />
